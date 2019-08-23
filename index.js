aff3d5258732782b7f0d19117941a0dcbc9e58ed
Akshay <akshay.soni.8707@gmail.com>
2019-08-23 18:13:16 +0530

    npm added

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-window.location.href = "https://material-drawn.herokuapp.com/index.html";
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+http.createServer(function(request, response) {
+
+    console.log('request starting for ');
+    console.log(request);
+
+    var filePath = '.' + request.url;
+    if (filePath == './')
+        filePath = './index.html';
+
+    console.log(filePath);
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+    }
+
+    path.exists(filePath, function(exists) {
+
+        if (exists) {
+            fs.readFile(filePath, function(error, content) {
+                if (error) {
+                    response.writeHead(500);
+                    response.end();
+                } else {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                }
+            });
+        } else {
+            response.writeHead(404);
+            response.end();
+        }
+    });
+
+}).listen(process.env.PORT || 5000);
+
+console.log('Server running at http://127.0.0.1:5000/');
